eff0a827ebde2f2d563168d6f7a0427376a66503
icaro <icaro@cliqx.com.br>
2021-11-15 19:46:07 -0300

    Primeiro commit

--- AppFlyout/ViewModels/ItemDetailViewModel.cs
+++ AppFlyout/ViewModels/ItemDetailViewModel.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 namespace AppFlyout.ViewModels
 {
     [QueryProperty(nameof(ItemId), nameof(ItemId))]
-    public class ItemDetailViewModel : BaseViewModel
+    public class ItemDetailViewModel : BaseViewModel 
     {
         private string itemId;
         private string text;
